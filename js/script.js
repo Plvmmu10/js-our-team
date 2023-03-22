64343927090e210196d6c4150ed6072007399c7e
Plvmmu10 <gluca0260@gmail.com>
2023-03-22 14:08:37 +0100

    created team array

---
+++ js/script.js
@@ -0,0 +1,57 @@
+//Creare l’array di oggetti con le informazioni fornite.
+//Stampare su console, per ogni membro del team, le informazioni di nome, ruolo e la stringa della foto
+//Stampare le stesse informazioni su DOM sottoforma di stringhe
+//Trasformare la stringa foto in una immagine effettiva
+//Organizzare i singoli membri in card/schede
+
+
+const teamArr = 
+
+[
+
+    {
+        'userName' : 'Wayne',
+        'surname' : 'Barnett',
+        'role': 'Founder & CEO',
+        'img':  'wayne-barnett-founder-ceo.jpg'
+    },
+
+    {
+        'userName' : 'Angela',
+        'surname' : 'Caroll',
+        'role': 'Chief Editor',
+        'img':  'angela-caroll-chief-editor.jpg'
+    },
+
+    {
+        'userName' : 'Walter',
+        'surname' : 'Gordon',
+        'role': 'Office Manager',
+        'img':  'walter-gordon-office-manager.jpg'
+    },
+
+    {
+        'userName' : 'Angela',
+        'surname' : 'Lopez',
+        'role': 'Social Media Manager',
+        'img':  'angela-lopez-social-media-manager.jpg'
+    },
+
+    {
+        'userName' : 'Scott',
+        'surname' : 'Estrada',
+        'role': 'Developer',
+        'img':  'scott-estrada-developer.jpg'
+    },
+
+    {
+        'userName' : 'Barbara',
+        'surname' : 'Ramos',
+        'role': 'Graphic Designer',
+        'img':  'barbara-ramos-graphic-designer.jpg'
+    }
+
+
+
+
+]
